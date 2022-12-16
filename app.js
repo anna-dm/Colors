ce69563e363f22b058c965d12ec96c2f05a4f72f
anna-dm <anna-anna-frolova@bk.ru>
2022-12-16 23:03:06 +0300

    second

--- app.js
+++ app.js
@@ -17,11 +17,27 @@ function generateRandomeColor() {
 
 function setRandomColors() {
   cols.forEach((col) => {
-    col.style.background = generateRandomeColor()
+    const text = col.querySelector('h2')
+    const color = chroma.random()
+
+    text.textContent = color
+    col.style.background = color
+
+    setTextColor(text, color)
 
   })
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
+
+
+
 
 }
+
+
 
 
 setRandomColors()
